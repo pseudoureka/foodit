df056fd0990bed5485968cec0edc933f42763fae
pseudoureka <messiah7179@gmail.com>
2024-10-17 18:00:54 +0900

    파일인풋 적용

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import "./FoodForm.css";
+import FileInput from "./FileInput";
 
 const INITIAL_VALUE = {
   title: "",
   calorie: 0,
   content: "",
+  imgFile: null,
 };
 
 function FoodForm() {
   const [values, setValues] = useState(INITIAL_VALUE);
 
-  const handleChange = (e) => {
-    const { name, value, type } = e.target;
+  const handleChange = (name, value, type) => {
     setValues((prevValues) => ({
       ...prevValues,
       [name]: sanitize(type, value),
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    handleChange(name, value, type);
   };
 
   function sanitize(type, value) {
@@ -31,6 +37,7 @@ function FoodForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
+    console.log(typeof values.calorie);
     setValues(INITIAL_VALUE);
   };
 
@@ -38,11 +45,12 @@ function FoodForm() {
 
   return (
     <form className="FoodForm" onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange} />
-      <input name="calorie" value={values.calorie} type="number" onChange={handleChange} />
-      <textarea name="content" value={values.content} onChange={handleChange} />
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input name="calorie" value={values.calorie} type="number" onChange={handleInputChange} />
+      <textarea name="content" value={values.content} onChange={handleInputChange} />
       <button type="submit" disabled={!isFormValid}>
-        보내기
+        저장
       </button>
     </form>
   );
