e3618475071f79e0ea1b27a36db55fcd815e6aed
pseudoureka <messiah7179@gmail.com>
2024-10-23 21:41:24 +0900

    수정 버튼 만들기, 수정 버튼 클릭시 foodform 랜더링하기

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./FoodList.css";
+import FoodForm from "./FoodForm";
 
 function formatDate(value) {
   const date = new Date();
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function FoodListItem({ item, onDelete }) {
+function FoodListItem({ item, onDelete, onEdit }) {
   const { imgUrl, title, calorie, content, createdAt } = item;
+
   const handleDeleteClick = () => onDelete(item.id);
+  const handleEditClick = () => onEdit(item.id);
 
   return (
     <div className="FoodListItem">
@@ -16,19 +20,31 @@ function FoodListItem({ item, onDelete }) {
       <div>칼로리 : {calorie}</div>
       <div>{content}</div>
       <div>{formatDate(createdAt)}</div>
+      <button onClick={handleEditClick}>수정</button>
       <button onClick={handleDeleteClick}>삭제</button>
     </div>
   );
 }
 
 function FoodList({ items, onDelete }) {
+  const [editingId, setEditingId] = useState();
+
   return (
     <ul className="FoodList">
-      {items.map((item) => (
-        <li key={item.id}>
-          <FoodListItem item={item} onDelete={onDelete} />
-        </li>
-      ))}
+      {items.map((item) => {
+        if (item.id === editingId) {
+          return (
+            <li key={item.id}>
+              <FoodForm />
+            </li>
+          );
+        }
+        return (
+          <li key={item.id}>
+            <FoodListItem item={item} onDelete={onDelete} onEdit={setEditingId} />
+          </li>
+        );
+      })}
     </ul>
   );
 }
